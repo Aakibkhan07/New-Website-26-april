17c6ed3c2ba1e539c695b97f3e3250f11ca8c76d
Aakib khan <152098669+Aakibkhan07@users.noreply.github.com>
2026-05-04 18:13:42 +0530

    Update create-order.js

--- trademetrix-v6/api/create-order.js
+++ trademetrix-v6/api/create-order.js
@@ -1,8 +1,8 @@
 const Razorpay = require("razorpay");
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.rzp_test_SlHX4L1Fozb0v3,
+  key_secret: process.env.YvK7oJPy54O1bYkBz6R703mz,
 });
 
 module.exports = async function handler(req, res) {
